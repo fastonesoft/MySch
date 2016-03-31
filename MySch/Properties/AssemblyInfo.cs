3ce534d1a96f61b92b80ebaa149b61b565a0a464
fastonesoft <2347632@qq.com>
2016-03-31 20:54:50 +0800

    2

--- MySch/Properties/AssemblyInfo.cs
+++ MySch/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("eb8d5a29-deb3-48e4-86c1-1ceb2d2aea8f")]
+[assembly: Guid("8c727402-8e19-41f5-b49f-f9cb53025cec")]
 
 // 程序集的版本信息由下列四个值组成:
 //
