c397eb0fccc7f01c2119cf1eed4dedab63497491
fastonesoft <2347632@qq.com>
2016-03-31 18:11:37 +0800

    1

---
+++ MySch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySch.Startup))]
+namespace MySch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
