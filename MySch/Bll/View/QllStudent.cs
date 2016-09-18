fb505a3988e57995e450ae055c9732919a3c8d7e
fastonesoft <2347632@qq.com>
2016-09-18 12:52:44 +0800

    9.18.1

---
+++ MySch/Bll/View/QllStudent.cs
@@ -0,0 +1,25 @@
+﻿using MySch.Dal;
+using MySch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MySch.Bll.View
+{
+    public class QllStudent : BllBase<QStudent>
+    {
+        public string ID { get; set; }
+        public string IDS { get; set; }
+        public string Name { get; set; }
+        public string CID { get; set; }
+        public string PartStepIDS { get; set; }
+        public string OutIDS { get; set; }
+        public bool Checked { get; set; }
+        public string AccIDS { get; set; }
+        public string OpenID { get; set; }
+        public string OutName { get; set; }
+        public string PartStepName { get; set; }
+    }
+}
