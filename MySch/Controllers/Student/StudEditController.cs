9f7fa2c842bfa753f0f38805ce8dd600572d1afd
fastonesoft <2347632@qq.com>
2016-10-19 15:18:26 +0800

    10.19.1

---
+++ MySch/Controllers/Student/StudEditController.cs
@@ -0,0 +1,32 @@
+﻿using MySch.Bll;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySch.Controllers.Student
+{
+    public class StudEditController : RoleStudController
+    {
+        //学生注册首页
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Query()
+        {
+            return View();
+        }
+
+    }
+}
