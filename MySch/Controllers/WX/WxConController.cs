027af80da85cf5d2c2a2afc8a4b0dd262d02e341
fastonesoft <2347632@qq.com>
2017-06-21 00:17:29 +0800

    170621

---
+++ MySch/Controllers/WX/WxConController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySch.Controllers.WX
+{
+    public class WxConController : BaseController
+    {
+        // GET: WxCon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
