d8e754ab37b08cdfcaf2a1b06771c4fbbcc585f0
fastonesoft <2347632@qq.com>
2017-08-23 18:01:38 +0800

    17082301

--- MySch/Mvvm/Web/User/Act/ActAcc.cs
+++ MySch/Mvvm/Web/User/Act/ActAcc.cs
@@ -9,19 +9,17 @@ namespace MySch.Mvvm.Web.User.Act
 {
     public class ActAcc
     {
-        public static dynamic AccBanMaster(string ids, string unionid)
+        public static IEnumerable<VqAccBan> AccBanMaster(string ids, string unionid)
         {
             try
             {
-                var acc = VqAccBan.GetEntitys<VqAccBan>(a => a.ParentID == unionid);
                 var bans = DataCRUD<QrBanCurrent>.Entitys(a => a.AccIDS == ids && a.IsCurrent.HasValue && a.IsCurrent.Value);
-
                 var banstr = string.Empty;
                 foreach (var ban in bans)
                 {
                     banstr += (string.IsNullOrEmpty(ban.MasterIDS) ? "" : ban.MasterIDS + ",");
                 }
-                return acc.Select(a => !banstr.Contains(a.ID) && a.Passed && !a.Fixed);
+                return VqAccBan.GetEntitys<VqAccBan>(a => a.ParentID == unionid && a.Passed && !a.Fixed && !banstr.Contains(a.ID));
             }
             catch (Exception e)
             {
